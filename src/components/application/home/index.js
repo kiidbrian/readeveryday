79f2498c411428132574e68023adfe74c6762d3c
kiidbrian <brnpaintsil@gmail.com>
2017-01-01 10:16:03 +0000

    add assets
    complete site layout

--- src/components/application/home/index.js
+++ src/components/application/home/index.js
@@ -4,13 +4,7 @@ class Home extends Component {
   render() {
     return(
       <div>
-        <div className="teaser" style={
-          {
-            backgroundColor: "transparent",
-            backgroundImage: "url('img/shutterstock.jpg')",
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "100% auto",
-          }}>
+        <div className="teaser">
           <div className="container">
             <div className="text-logo">
               <p>Read</p>
@@ -20,37 +14,80 @@ class Home extends Component {
           </div>
         </div>
 
-        <section className="">
-          <div className="container">
-            <h1 className="section-title">About Us</h1>
+        <section className="about-us">
+        <h1 className="section-title">About Us</h1>
+        <div className="section-content">
+          <p>Read Every Day (RED) is a kid driven non-profit organization working to ensure high reading interests among
+            children pre/post kindergarten. RED was registered as a non-profit in 2017 with a highly efficient outreach program
+            reaching many kids by either placing a RED kiosk, a kid's friendly bookshelf in every classroom, community or facility
+            reading through RED Templates, a simple guide for parents and teachers. We focus not only engaging kids but parents particularly
+            as the US Department of Education research show that where parents are highly engaged, the reading scores of kids are at least
+            above 28% of the national average and 46% below national average where parents are less engaged, suggesting a 74 point gap</p>
+        </div>
+        </section>
 
-            <div className="section-content">
-              <p>Read Every Day (RED) is a kid driven non-profit organization working to ensure high reading interests among
-                children pre/post kindergarten. RED was registered as a non-profit in 2017 with a highly efficient outreach program
-                reaching many kids by either placing a RED kiosk, a kid's friendly bookshelf in every classroom, community or facility
-                reading through RED Templates, a simple guide for parents and teachers. We focus not only engaging kids but parents particularly
-                as the US Department of Education research show that where parents are highly engaged, the reading scores of kids are at least
-                above 28% of the national average and 46% below national average where parents are less engaged, suggesting a 74 point gap</p>
+        <section className="our-vision">
+        <div className="row">
+        <div className="col-md-6">
+            <div className="tron-left">
+              <img src="img/shutterstock_4.png" />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="tron-right">
+            <h1 className="tron-title">Our Vision</h1>
+            <p className="text-white font-styles">
+              Our vision is to improve global opportunities for kids through reading.
+              Reading helps children develop vital language skills necessary for expressing themselves verbally;
+              it opens up new worlds; enriches children's lives through diversity of thought;
+              improves their fine motor skills and helps them develop future interest.
+            </p>
+            </div>
+          </div>
+        </div>
+        </section>
+
+        <section className="our-mission">
+          <div className="teaser-image"></div>
+          <div className="row">
+            <div className="col-md-5 col-md-offset-7">
+              <h1 className="section-title">Our Mission</h1>
+              <p className="section-content">
+                Our mission is to increase reading interest
+                among kids in every corner of the globle
+              </p>
             </div>
           </div>
         </section>
 
-        <section className="">
-          <div className="container">
-            <div className="col-md-6"></div>
-            <div className="col-md-6 container">
+        <section className="our-team">
+          <div className="our-team-box">
+            <h1 className="section-title">Our Team</h1>
+            <p className="section-content">
+              RED in partnership with volunteers and committed personnel is able to diversify funding sources and continue
+              to promote reading across the globe. Our reading partners throughout the United States and Africa including community
+              centers/churches, early childhood programs, health centers, schools, migrant communities and/or special needs facilities make
+              our mission of reaching kids across the globe possible.
+            </p>
+          </div>
 
+
+          <div className="row team-members">
+            <div className="col-md-12">
+              <div className="col-md-4 image-holder-1">
+
+              </div>
+
+              <div className="col-md-4 image-holder-2">
+
+              </div>
+
+              <div className="col-md-4 image-holder-3">
+
+              </div>
             </div>
           </div>
-        </section>
 
-        <section className="">
-          <div className="jumboman">
-            <div className="col-md-6"></div>
-            <div className="col-md-6">
-
-            </div>
-          </div>
         </section>
       </div>
     );
--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -5,8 +5,53 @@ class Footer extends Component {
   render() {
     return(
       <footer>
-        <hr/>
-        <p>&copy; Company 2018</p>
+        <div className="container">
+          <div className="col-md-4">
+            <h1 className="text-footer">Contact</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+
+          <div className="col-md-8">
+            <div className="row">
+                <div className="col-md-6">
+                  <input id="name" className="input-item" placeholder="Your Name" size=""/>
+                </div>
+                <div className="col-md-6">
+                  <input id="subject" className="input-item" placeholder="Subject" size=""/>
+                </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12 p-t-50">
+                <input id="message" className="input-item-1" placeholder="Say hello" size=""/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-4 p-t-40">
+                <button className="btn btn-default send-now">Send Now </button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container p-t-150">
+          <hr />
+          <div className="row">
+            <div className="col-md-6">
+              <p className="copyright">All Rights Reserved &copy; ReadEveryday (RED) 2017-2018</p>
+            </div>
+            <div className="col-md-6">
+              <ul className="list-inline pull-right text-red">
+                  <li><i className="fa fa-facebook-f"/></li>
+                  <li><i className="fa fa-twitter"/></li>
+                  <li><i className="fa fa-google-plus"/></li>
+                  <li><i className="fa fa-instagram"/></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </footer>
     );
   }
