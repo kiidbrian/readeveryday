626d2ec27772805fd16b6bcd35f9cb0141f95188
kiidbrian <brnpaintsil@gmail.com>
2018-01-25 00:17:30 +0000

    navbar opacity cleanup

--- src/components/application/home/index.js
+++ src/components/application/home/index.js
@@ -1,24 +1,57 @@
 import React, {Component} from 'react';
 
 class Home extends Component {
-  constructor(props){
-    super(props)
-  }
-
   render() {
     return(
-      <div className="jumbotron" style={
-        {
-          backgroundColor: "transparent",
-          backgroundImage: "url('img/shutterstock.jpg')",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center center",
-          backgroundSize: "100% auto"}}>
-        <div className="container">
-          <p>Read</p>
-          <p>Every</p>
-          <p>Day</p>
+      <div>
+        <div className="teaser" style={
+          {
+            backgroundColor: "transparent",
+            backgroundImage: "url('img/shutterstock.jpg')",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "100% auto",
+          }}>
+          <div className="container">
+            <div className="text-logo">
+              <p>Read</p>
+              <p>Every</p>
+              <p>Day</p>
+            </div>
+          </div>
         </div>
+
+        <section className="">
+          <div className="container">
+            <h1 className="section-title">About Us</h1>
+
+            <div className="section-content">
+              <p>Read Every Day (RED) is a kid driven non-profit organization working to ensure high reading interests among
+                children pre/post kindergarten. RED was registered as a non-profit in 2017 with a highly efficient outreach program
+                reaching many kids by either placing a RED kiosk, a kid's friendly bookshelf in every classroom, community or facility
+                reading through RED Templates, a simple guide for parents and teachers. We focus not only engaging kids but parents particularly
+                as the US Department of Education research show that where parents are highly engaged, the reading scores of kids are at least
+                above 28% of the national average and 46% below national average where parents are less engaged, suggesting a 74 point gap</p>
+            </div>
+          </div>
+        </section>
+
+        <section className="">
+          <div className="container">
+            <div className="col-md-6"></div>
+            <div className="col-md-6 container">
+
+            </div>
+          </div>
+        </section>
+
+        <section className="">
+          <div className="jumboman">
+            <div className="col-md-6"></div>
+            <div className="col-md-6">
+
+            </div>
+          </div>
+        </section>
       </div>
     );
   }
--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -2,14 +2,10 @@ import React,{Component} from 'react';
 import {Link} from 'react-router-dom';
 
 class Header extends Component {
-  constructor(props){
-    super(props)
-  }
-
   render() {
     return(
       <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div className="container">
+        <div className="container-fluid">
           <div className="navbar-header">
             <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
               <span className="sr-only">Toggle navigation</span>
@@ -20,7 +16,7 @@ class Header extends Component {
           </div>
           <div id="navbar" className="navbar-collapse collapse pull-right">
             <ul className="nav navbar-nav">
-              <li className="active"><a href="#">HOME <span className="sr-only">(current)</span></a></li>
+              <li><a href="#">HOME <span className="sr-only">(current)</span></a></li>
               <li><a href="#">ABOUT</a></li>
               <li><a href="#">SERVICES</a></li>
               <li><a href="#">CONTACT</a></li>
